c8f72bab003f528af33bd1a2417fa825f1f7a0dc
V01CE07 <vaiper5123848@gmail.com>
2024-06-17 19:04:46 +0300

    lul
    I'm starting to make the functionality
    first - switching between layouts

--- drozdovApp2/app/src/main/java/com/example/drozdovapp/MainActivity.kt
+++ drozdovApp2/app/src/main/java/com/example/drozdovapp/MainActivity.kt
@@ -1,20 +1,32 @@
 package com.example.drozdovapp
 
+import android.content.Intent
 import android.os.Bundle
+import android.widget.Button
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(R.layout.activity_main)
+        val button = findViewById<Button>(R.id.firstTryBtn)
+        button.setOnClickListener{
+            val intent = Intent(this, activity_onboarding::class.java)
+            startActivity(intent)
+        }
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
+
     }
+
+
+
 }
--- drozdovApp2/app/src/main/java/com/example/drozdovapp/activity_onboarding.kt
+++ drozdovApp2/app/src/main/java/com/example/drozdovapp/activity_onboarding.kt
@@ -1,20 +1,45 @@
 package com.example.drozdovapp
 
+import android.content.Intent
+import android.media.Image
 import android.os.Bundle
+import android.widget.Button
+import android.widget.ImageButton
+import android.widget.TextView
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+
+
 
 class activity_onboarding : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(R.layout.activity_onboarding)
+
+        val toRegisterBtn = findViewById<Button>(R.id.toRegisterBtn)
+        val toLoginBtn = findViewById<ImageButton>(R.id.loginButton)
+
+        toRegisterBtn.setOnClickListener{
+            val intent = Intent(this, reg_window::class.java)
+            startActivity(intent)
+        }
+
+        toLoginBtn.setOnClickListener{
+            val intent = Intent(this, activity_login::class.java)
+            startActivity(intent)
+        }
+
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
+
+
         }
+
+
     }
 }
